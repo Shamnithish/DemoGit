49c869711d40f7beed9f6f465cfd1e9d7ac76612
RajeshV99 <vrajeeesh1999@gmail.com>
2022-12-23 11:01:37 +0530

    FirstCommit

---
+++ src/main/java/Gitdemo/BasicDemo/App.java
@@ -0,0 +1,13 @@
+package Gitdemo.BasicDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Gitdemo/BasicDemo/AppTest.java
@@ -0,0 +1,20 @@
+package Gitdemo.BasicDemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
